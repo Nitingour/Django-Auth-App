8fd26c26484f46a79abbd3b668d41a65629a7a7d
EdSystango <nitingour99@gmail.com>
2019-05-06 16:32:51 +0530

    final commit

---
+++ BlogApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from BlogApp.forms import SignupForm
+# Create your views here.
+def index(request):
+    return render(request,'BlogApp/index.html')
+
+def newblog(request):
+    return render(request,'BlogApp/newblog.html')
+
+def viewblogs(request):
+    return render(request,'BlogApp/viewblog.html')
+
+def signupview(request):
+    if request.method=='GET':
+        sform=SignupForm()
+        return render(request,"BlogApp/signup.html",{'sform':sform})
+    if request.method=='POST':
+        sform=SignupForm(request.POST)
+        user=sform.save()
+        user.set_password(user.password)
+        user.save()
+        return render(request,"BlogApp/success.html",{'msg':'DONE'})
